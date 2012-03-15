9759b4084fc86176bd927adb97c6fe54c87f3f61
Yusuke Suzuki <yusuke.harmonia.suzuki@gmail.com>
2012-03-15 12:25:23 +0900

    Add math/constants and the compile test

---
+++ modules/math/include/matcha/math/constants.hpp
@@ -0,0 +1,37 @@
+/*
+ * Matcha Robotics Application Framework
+ *
+ * Copyright (C) 2011 Yusuke Suzuki 
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#ifndef MATCHA_MATH_CONSTANTS_HPP__
+#define MATCHA_MATH_CONSTANTS_HPP__
+
+namespace matcha { namespace math {
+
+inline constexpr double sqrt_pi()
+{
+	return 1.772453850905515881919427556567825376987457275390625;
+}
+
+inline constexpr double sqrt_2_pi()
+{
+	return 2.506628274631000241612355239340104162693023681640625;
+}
+
+} // end of namespace math
+} // end of namespace matcha
+
+#endif
+
